240970cc1ac95989becadb3cadbb90c07bef465c
rushikeshbhandare <86002633+rushikeshbhandare97@users.noreply.github.com>
2021-08-31 16:30:55 +0530

    Create EmpWage.js

---
+++ EmpWage.js
@@ -0,0 +1,21 @@
+//Refactored code to write function for daily working hours
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+function getWorkingHours(empCheck) {
+    switch (empCheck) {
+      case IS_PART_TIME:
+        return PART_TIME_HOURS;
+      case IS_FULL_TIME:
+        return FULL_TIME_HOURS;
+      default:
+        return 0;
+    }
+  }
+  
+  let empCheck = Math.floor(Math.random() * 10) % 3;
+  let empHours = getWorkingHours(empCheck);
+  let empWage = empHours * WAGE_PER_HOUR;
+  console.log("Employee Wage " + empWage);
